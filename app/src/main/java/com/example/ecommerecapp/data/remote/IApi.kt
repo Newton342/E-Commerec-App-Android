c6180322c6ae7e527bd20d4a7e2e23435032e12f
pratik <pratikspawar45@gmail.com>
2025-12-18 15:05:36 +0000

    Product API route and function added in interface

--- app/src/main/java/com/example/ecommerecapp/data/remote/IApi.kt
+++ app/src/main/java/com/example/ecommerecapp/data/remote/IApi.kt
@@ -1,14 +1,19 @@
 package com.example.ecommerecapp.data.remote
 
 import com.example.ecommerecapp.domain.model.LoginModel
+import com.example.ecommerecapp.domain.model.ProductModel
 import com.example.ecommerecapp.domain.model.TokenModel
 import com.example.ecommerecapp.domain.model.User
+import com.example.ecommerecapp.network.CATEGORIES
 import com.example.ecommerecapp.network.LOGIN
+import com.example.ecommerecapp.network.PRODUCT
+import com.example.ecommerecapp.network.PRODUCTS
 import com.example.ecommerecapp.network.USERS
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface IApi {
     @GET(USERS)
@@ -16,4 +21,13 @@ interface IApi {
 
     @POST(LOGIN)
     suspend fun login(@Body login: LoginModel): Response<TokenModel>
+
+    @GET(PRODUCTS)
+    suspend fun getAllProducts(): Response<List<ProductModel>>
+
+    @GET(PRODUCT)
+    suspend fun getProductById(@Path("id") id: Int): Response<ProductModel>
+
+    @GET(CATEGORIES)
+    suspend fun getCategories(): Response<List<String>>
 }
--- app/src/main/java/com/example/ecommerecapp/network/HttpUrls.kt
+++ app/src/main/java/com/example/ecommerecapp/network/HttpUrls.kt
@@ -5,3 +5,10 @@ const val BASEURL = "https://fakestoreapi.com/"
 const val USERS = "users"
 
 const val LOGIN = "auth/login"
+
+const val PRODUCTS = "products"
+
+const val PRODUCT = "product/{id}"
+
+const val CATEGORIES = "products/categories"
+
